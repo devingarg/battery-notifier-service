0e32df4a143e6f105a3d88ccd6577e8875b97d00
Devin Garg <degarg@ucsd.edu>
2022-11-19 17:37:42 -0800

    beep only if it's plugged in

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from beepy import beep
 # get the battery object
 battery_obj = psutil.sensors_battery()
 
-# check for battery percent
+# retrieve battery charge
 percent = battery_obj.percent
 
-if percent > 40:
+# retrieve plug status
+plugged_in= battery_obj.power_plugged
+
+if percent > 90 and plugged_in:
     beep(sound = 'ready')
